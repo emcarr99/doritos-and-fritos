8a6ca61639ff9c7654d7f4dd45562a0218890b56
Emma Carr <lorenagracex@gmail.com>
2023-05-06 15:23:40 -0500

    todos and lay out tables

---
+++ db/schema.sql
@@ -0,0 +1,20 @@
+-- TODO employee database
+-- TODO department table: id (INT), name (VARCHAR 30)
+-- TODO role table: id (INT), title (VARCHAR 30), salary (decimal), department_id (INT)
+-- TODO employee table: id (INT), first_name (VARCHAR 30), last_name (VARCHAR 30), role_id (INT), manager_id (INT)
+-- only use drop in a school setting (always get confirmation to drop or delete in a work setting)
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE employees;
+
+USE employees;
+
+CREATE TABLE department ()
+
+
+CREATE TABLE role ()
+
+
+
+CREATE TABLE employee ()
+
+
