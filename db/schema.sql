06b808085f963d57a00611d7d74298bf607d2027
Emma Carr <lorenagracex@gmail.com>
2023-05-06 15:32:09 -0500

    tables created

--- db/schema.sql
+++ db/schema.sql
@@ -8,13 +8,27 @@ CREATE DATABASE employees;
 
 USE employees;
 
-CREATE TABLE department ()
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) UNIQUE NOT NULL
+);
 
 
-CREATE TABLE role ()
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) UNIQUE NOT NULL,
+  salary INT NOT NULL,
+  department_id INT NOT NULL,
+)
 
-
-
-CREATE TABLE employee ()
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NULL,
+  PRIMARY KEY (id),
+  foreign key manager_id references employee(id)
+)
 
 
