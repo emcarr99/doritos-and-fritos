9b29c7cd3f8f93460d942abf86cfae14bc48f2aa
Emma Carr <lorenagracex@gmail.com>
2023-05-06 16:30:57 -0500

    menu prompts

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 const inquirer = require("inquirer");
+// require inquirer 
+require('console.table');
+
+const employeeDb = require ('./db/queries');
+
+const userPrompts = [
+  {
+    type: 'list',
+    name: 'menuChoice',
+    message: 'What do you want to do?',
+    choices: [
+      {
+        name: 'View All Departments',
+        value: 1
+      },
+      {
+        name: 'View All Roles',
+        value: 2
+      },
+      {
+        name: 'View All Employees',
+        value: 3
+      },
+      {
+        name: 'Add Department',
+        value: 4
+      },
+      {
+        name: 'Add Role',
+        value: 5
+      },
+      {
+        name: 'Add Employee',
+        value: 6
+      },
+      {
+        name: 'Update Employee Role',
+        value: 7
+      },
+      {
+        name: 'Quit',
+        value: 8
+      }
+    ]
+  }
+];
